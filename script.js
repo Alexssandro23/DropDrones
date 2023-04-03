d0de4a8e684c8c0383b3c26b3e8b75bb74c27acb
alex <allexbottelho2003@gmail.com>
2023-04-03 17:18:18 -0300

    modelo de rodapé adiocinado resposividade inserida usando @media

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function trocarImg(e){
-    document.getElementById('trocar-1').src=e;
+// function trocarImg(e){
+//     document.getElementById('trocar-1').src=e;
 
+// }
+const imgs =document.getElementById("img");
+const img=document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function slide(){
+    idx++;
+    if(idx>img.length-1){
+        idx=0;
+    }
+    imgs.style.transform = `translateX(${-idx * 385}px)`;
+    console.log('entrou');
 }
+setInterval(slide, 3100);
